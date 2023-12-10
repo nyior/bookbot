5d43cf73db08c78d800f983c9685a617e27c74fa
Nyior <nyior.clement@aun.edu.ng>
2023-12-10 14:50:03 +0100

    Fix string return format

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ from collections import Counter
 
 def num_of_words(content):
     words_count = len(content.split())
-    letters_count  =  count_letters(content)
-
-    print("--- Begin report of books/frankenstein.txt ---\n\n")
-    print(f"{words_count} words found in the document")
-    
-    keys = list(letters_count)
-    
-    for c in keys:
-        if c.isalpha():
-            print(f"The {c} character was found {letters_count[c]} times")
+    return words_count
 
 
 def count_letters(content):
     # A pythonic approach
-    # col = Counter(content)
-    # return dict(col)  
+    col = Counter(content)
+    return dict(col)  
 
     # A more verbose approach
-    chars = {}
-    for c in content:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
+    # chars = {}
+    # for c in content:
+    #     lowered = c.lower()
+    #     if lowered in chars:
+    #         chars[lowered] += 1
+    #     else:
+    #         chars[lowered] = 1
+    # return chars
     
 
 with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    words_count = num_of_words(file_contents.lower())
-    print(words_count)
+    text = f.read()
+    words_count = num_of_words(text.lower())
+
+    print("--- Begin report of books/frankenstein.txt ---\n\n")
+    print(f"{words_count} words found in the document")
+
+    chars_count  =  count_letters(text)
+    keys = list(chars_count.keys())
+    keys.sort()
+    for c in keys:
+        if c.isalpha():
+            print(f"The {c} character was found {chars_count[c]} times")
